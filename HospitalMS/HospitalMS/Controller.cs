9c42f226f1ed78e50b95b593800091657a0424bd
brooky56 <aidarahmetshin56@gmail.com>
2019-04-18 03:06:11 +0300

    Sign In/Up pages are added

---
+++ HospitalMS/HospitalMS/Controller.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HospitalMS
+{
+    class Controller
+    {
+        public Controller()
+        {
+           
+        }
+        public void CheckNumericValuesTextBox(TextBox textBox) {
+            foreach (char c in textBox.Text.ToCharArray())
+            {
+                if (!char.IsDigit(c))
+                {
+                    MessageBox.Show("This textbox accepts only numbers.");
+                    textBox.Clear();
+                    break;
+                }
+            }
+        }
+        public bool CheckPasswordBox(PasswordBox passwordBox)
+        {
+            bool flag = false;
+            if (!System.Text.RegularExpressions.Regex.IsMatch(passwordBox.Password, "[a-zA-Z]+$"))
+            {
+                flag = true;
+            }
+            else
+            {
+                MessageBox.Show("Such character is not available.");
+                passwordBox.Clear();
+            }
+            return flag;
+        }
+        public string ReturnStringFromTextBox(TextBox textBox)
+        {
+            string input = "";
+            if (textBox.Text != null)
+            {
+                input = textBox.Text;
+            }
+            return input;
+        }
+        public string ReturnPasswordFromTextBox(PasswordBox passwordBox)
+        {
+            string password = "";
+            if (passwordBox.Password != null)
+            {
+                password = passwordBox.Password;
+            }
+            return password;
+        }
+
+        public void CheckLiteralValuesTextBox(TextBox textBox)
+        {
+            if (textBox.Text.Length != 0)
+                if (!System.Text.RegularExpressions.Regex.IsMatch(textBox.Text, "^[a-zA-Z]"))
+                {
+                    MessageBox.Show("Accepts only Latin characters.");
+                    textBox.Clear();
+                }
+        }
+    }
+}
---
+++ HospitalMS/HospitalMS/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HospitalMS
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Controller controller;
+        public MainWindow()
+        {
+            InitializeComponent();
+            controller = new Controller();
+        }
+
+        private void SignInButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SignOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            LogInStackPanel.Visibility = Visibility.Hidden;
+            this.Title = "Sign Up"; 
+            SignUpStackPanel.Visibility = Visibility.Visible;
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            SignUpStackPanel.Visibility = Visibility.Hidden;
+            this.Title = "Log In";
+            LogInStackPanel.Visibility = Visibility.Visible;
+        }
+
+        private void LogInTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            controller.CheckLiteralValuesTextBox(LoginTextBox);
+        }
+    }
+}
